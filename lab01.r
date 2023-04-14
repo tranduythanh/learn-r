fe2fffa96d1af741ab938678ce2589a405173ad8
tranduythanh <coachtranduythanh@gmail.com>
2023-04-14 07:34:53 +0700

    yeah

--- lab01.r
+++ lab01.r
@@ -35,7 +35,7 @@ print(cardinality_ordered_distinct_combinations)
 
 # Sinh tất cả codon có 3 nucleotic khác nhau không phân biệt thứ tự
 unordered_distinct_combinations <- combinations(n = length(elements), r = n, v = elements, repeats.allowed = FALSE)
-print(distinct_combinations)
+print(unordered_distinct_combinations)
 cardinality_unordered_distinct_combinations <- factorial(N)/(factorial(N-n)*factorial(n))
 print(cardinality_unordered_distinct_combinations)
 
@@ -61,8 +61,36 @@ print(ordered_distinct_combinations)
 
 # Sinh tất cả codon có 3 nucleotic khác nhau không phân biệt thứ tự
 unordered_distinct_combinations <- urnsamples(x=elements, size=n, replace=FALSE, ordered=FALSE)
-print(distinct_combinations)
+print(unordered_distinct_combinations)
 
 # Sinh tất cả codon có thể lặp lại và không phân biệt thứ tự
 unordered_combinations <- urnsamples(x=elements, size=n, replace=TRUE, ordered=FALSE)
 print(unordered_combinations)
+
+
+
+
+
+# Bài 2.2
+
+# Hộp có 3 banh đỏ, 2 banh trắng
+Box <- c(rep('red', 3), rep('white',5))
+
+print(Box)
+Omega <- urnsamples(Box,2, replace = FALSE, ordered = FALSE)
+print(Omega)
+KGXS <- probspace(Omega)
+print(KGXS)
+eprint(KGXS)
+A <- subset(KGXS, X1=='red'.  & X2=='red')
+print(A)
+B <- subset(KGXS, X1=='white' & X2=='white')
+print(B)
+C <- subset(KGXS, X1=='red'   & X2=='white')
+print(C)
+Prob(A)
+Prob(B)
+Prob(C)
+sum(A$probs)
+sum(B$probs)
+sum(C$probs)
